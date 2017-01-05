40d0875de026ae4ead31d356cfb69b9ef5a60b43
Rosdyana Kusuma <rosdyana.kusuma@gmail.com>
2017-01-06 00:13:59 +0800

    - modify comment

--- Problem_H/Python/ProblemH/ProblemH.py
+++ Problem_H/Python/ProblemH/ProblemH.py
@@ -17,8 +17,8 @@ print(" |      __| _` | __ \  __ \  |   __ \   _ \  _ \   __| |   |")
 print(" |   | |   (   | |   | | | | |   | | |  __/ (   | |    |   |")
 print("\____|_|  \__,_| .__/ _| |_|_|  _| |_|\___|\___/ _|   \__, |")
 print("                _|                                    ____/ ")
-print("     Problem H - rosdyana.kusuma [at] gmail [dot] com")
-print("     2016 ACM ICPC Asia Chung-Li Regional\n")
+print("             rosdyana.kusuma [at] gmail [dot] com")
+print("     2016 ACM ICPC Asia Chung-Li Regional - Problem H\n")
 
 inputVertices1 = []
 inputVertices2 = []
@@ -46,17 +46,22 @@ if numOfGraphs <= 10 :
                     separateEdges = inputEdges.split(' ')
                     inputVertices1.append(int(separateEdges[0]))
                     inputVertices2.append(int(separateEdges[1]))
+
+                #normalizing vertices
                 normV1 = np.array(inputVertices1)
                 normV2 = np.array(inputVertices2)
                 for x,y in enumerate(normV1) :
                     normV1[x]=y-1
                 for x,y in enumerate(normV2) :
                     normV2[x]=y-6
+
+                #find the perfect matching , thanks to numpy
                 findMatching = (normV1==normV2).all()
                 if findMatching :                    
                     statPerfectmatching.append('0')
                 else :
                     statPerfectmatching.append('1')
+
             else : 
                 print 'Maximum number of edges in a bipartite graph is 46.\n'
         else :
